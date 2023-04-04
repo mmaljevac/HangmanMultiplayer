573be9e9bfd933463bda5237fa88070667bc0e34
Matija Maljevac <matijamaljevac@gmail.com>
2023-04-05 00:27:35 +0200

    Basics

--- src/main/java/hr/tvz/hangman/HangmanApplication.java
+++ src/main/java/hr/tvz/hangman/HangmanApplication.java
@@ -18,6 +18,7 @@ public class HangmanApplication extends Application {
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
--- src/main/java/hr/tvz/hangman/HangmanController.java
+++ src/main/java/hr/tvz/hangman/HangmanController.java
@@ -2,27 +2,55 @@ package hr.tvz.hangman;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.File;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class HangmanController {
+    private Integer numTries = 6;
+    private boolean gameOver = false;
+    private String word = "hangman";
 
     @FXML
     private ImageView imageView;
-
+    @FXML
+    private TextField letter;
+    @FXML
+    private Text numTriesText;
     @FXML
     private Button myButton;
 
-    @FXML
-    public void changeImage() {
 
+    @FXML
+    public void update() {
+        if (gameOver) return;
+
+        wrongLetter();
+
+    }
+
+    public void wrongLetter() {
+        numTriesText.setText(numTries.toString());
+
+        numTries--;
+
+        //        File file = new File("./img/" + numTries + ".png");
+//        Image image = new Image(file.toURI().toString());
+//        System.out.println(image.getUrl());
+//        System.out.println(imageView.getImage().getUrl());
+//        imageView.setImage(image);
+//        System.out.println(imageView.getImage().getUrl());
     }
 
 }
