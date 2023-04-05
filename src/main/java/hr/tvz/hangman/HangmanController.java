876684a95f189fc9e25be472a3929e4a74bc7909
Matija Maljevac <matijamaljevac@gmail.com>
2023-04-06 00:48:12 +0200

    Create game logic

--- src/main/java/hr/tvz/hangman/HangmanController.java
+++ src/main/java/hr/tvz/hangman/HangmanController.java
@@ -6,7 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import lombok.AllArgsConstructor;
@@ -15,6 +15,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 @AllArgsConstructor
@@ -28,11 +29,11 @@ public class HangmanController implements Initializable {
     @FXML
     private Text wordText;
     @FXML
-    private Text secretWordText;
+    private Text guessedWordText;
     @FXML
     private ImageView imageView;
     @FXML
-    private TextField letter;
+    private TextField letterField;
     @FXML
     //TODO to be replaced with images
     private Text livesText;
@@ -41,25 +42,76 @@ public class HangmanController implements Initializable {
 
     @FXML
     public void enterLetter(ActionEvent event) {
-        if (lives <= 0) {
-            gameOver = true;
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        if (!gameOver) {
+            checkLetter();
+            checkWin();
+            letterField.clear();
+        }
+        else {
+            System.out.println(lives);
+            //TODO start a new game
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Game over");
             alert.setHeaderText("Game over!");
-            alert.setContentText("Better luck next time!");
+            alert.setContentText("Start a new game?");
             alert.showAndWait();
             return;
         }
+    }
 
-        lives--;
-        livesText.setText(lives.toString());
+    public void checkLetter() {
 
-        //        File file = new File("./img/" + numTries + ".png");
-//        Image image = new Image(file.toURI().toString());
-//        System.out.println(image.getUrl());
-//        System.out.println(imageView.getImage().getUrl());
-//        imageView.setImage(image);
-//        System.out.println(imageView.getImage().getUrl());
+        if (letterField.getText().length() != 1) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("You can enter 1 letter only!");
+            alert.showAndWait();
+            letterField.clear();
+            return;
+        }
+
+        Character letter = letterField.getText().toUpperCase().charAt(0);
+        String word = wordText.getText();
+
+        if (guessedWordText.getText().indexOf(letter) != -1) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("You already guessed this letter!");
+            alert.showAndWait();
+            letterField.clear();
+            return;
+        }
+
+        if (word.indexOf(letter) != -1) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == letter) {
+                    String newSecretWord = guessedWordText.getText();
+                    newSecretWord = newSecretWord.substring(0, i) + letter + newSecretWord.substring(i + 1);
+                    guessedWordText.setText(newSecretWord);
+                }
+            }
+        }
+        else {
+            lives--;
+            livesText.setText(lives.toString());
+            if (lives <= 0) {
+                gameOver = true;
+            }
+        }
+
+    }
+
+    public void checkWin() {
+        if (wordText.getText().equals(guessedWordText.getText())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Game over");
+            alert.setHeaderText("You guessed the word!");
+            alert.setContentText("Well played!");
+            alert.showAndWait();
+            gameOver = true;
+        }
+    }
+
+    public void newGame() {
+        gameOver = false;
     }
 
     @Override
@@ -68,9 +120,16 @@ public class HangmanController implements Initializable {
         lives = MAX_LIVES;
         livesText.setText(lives.toString());
 
-        //TODO temporary word
-        wordText.setText("tes t");
-        String word = wordText.getText();
+//        TextInputDialog dialog = new TextInputDialog();
+//        dialog.setTitle("Input Dialog");
+//        dialog.setHeaderText("Enter a word:");
+//        dialog.setContentText("Word:");
+//
+//        Optional<String> wordDialog = dialog.showAndWait();
+//        String word = wordDialog.orElse("");
+        String word = "tes t";
+
+        wordText.setText(word.toUpperCase());
 
         String secretWord = "";
         for (int i = 0; i < word.length(); i++) {
@@ -81,6 +140,6 @@ public class HangmanController implements Initializable {
                 secretWord += "*";
             }
         }
-        secretWordText.setText(secretWord);
+        guessedWordText.setText(secretWord);
     }
 }
