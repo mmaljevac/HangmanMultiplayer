0a3eea348b09d340f8b3790534e58e4dc8c9483a
Matija Maljevac <matijamaljevac@gmail.com>
2023-04-27 00:15:22 +0200

    Set up server

--- src/main/java/hr/tvz/hangman/HangmanApplication.java
+++ src/main/java/hr/tvz/hangman/HangmanApplication.java
@@ -1,23 +1,62 @@
 package hr.tvz.hangman;
 
+import hr.tvz.hangman.model.ClientData;
+import hr.tvz.hangman.tcp.Server;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Optional;
 
 public class HangmanApplication extends Application {
+    public static String playerName;
+
+    public static Integer port;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HangmanApplication.class.getResource("hangman-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        stage.setTitle("Hangman!");
+        stage.setTitle(playerName);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        if(Optional.ofNullable(args[0]).isPresent()
+                && Optional.ofNullable(args[1]).isPresent()) {
+            System.out.println("Player name: " + args[0]);
+            playerName = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+        sendRequest();
+
         launch();
+    }
+
+    private static void sendRequest() {
+        // Closing socket will also close the socket's InputStream and OutputStream.
+        try (Socket clientSocket = new Socket(Server.HOST, Server.PORT)){
+            System.err.println("Client is connecting to " + clientSocket.getInetAddress() + ":" +clientSocket.getPort());
+
+            //sendPrimitiveRequest(clientSocket);
+            sendSerializableRequest(clientSocket);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendSerializableRequest(Socket client) throws IOException, ClassNotFoundException {
+        ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+        ClientData newClientData = new ClientData(playerName, port);
+        oos.writeObject(newClientData);
+        System.out.println("Server confirmation: " + ois.readObject());
     }
 }
---
+++ src/main/java/hr/tvz/hangman/tcp/Client.java
@@ -0,0 +1,4 @@
+package hr.tvz.hangman.tcp;
+
+public class Client {
+}
